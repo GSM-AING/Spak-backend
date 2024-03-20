ee19db0a24e4eac38ff21bdc904b1f2b805322a9
seonmooKang <sunmoo1108@gmail.com>
2024-03-20 16:48:38 +0900

    파일 재생성

---
+++ src/main/java/com/spak/AING/AingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spak.AING;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spak/AING/AingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spak.AING;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
